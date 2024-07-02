6e18c8d42db60b8f931fcbfc012b0e62562372ce
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-02 13:50:30 +0600

    added shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Button } from "./components/ui/button";
 import "./index.css";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div></div>
+    <Provider store={store}>
+      <div className="mx-auto my-20">
+        <Button variant="link">Click me</Button>
+        <h1 className="text-5xl">Hello</h1>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </Provider>
   </React.StrictMode>
 );
