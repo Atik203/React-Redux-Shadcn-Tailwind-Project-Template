ec035b529c981ab8807d0c0b5e84a23b5b41cc36
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-25 23:02:31 +0600

    updated for npm package

---
+++ bin/cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { execSync } = require('child_process');
+
+const runCommand = command => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit',input:'y\n' });
+  } catch (e) {
+    console.error('Failed to execute ${command}', e);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/Atik203/React-Redux-Shadcn-Tailwind-Project-Template ${repoName}`;
+const installDepsCommand = `cd ${repoName} && yarn`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
+
+console.log(
+  'Congratulations! You are ready. Follow the following commands to start'
+);
+console.log(`cd ${repoName} && yarn dev`);
